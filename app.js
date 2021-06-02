e9965b1d3048367cd2f6a944ca2853c865ac2e91
Brayan Urbina <burbinagomez@gmail.com>
2021-06-02 18:08:20 -0500

    fist deploy

--- app.js
+++ app.js
@@ -1,22 +1,85 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
+const mercadopago = require('mercadopago');
 var port = process.env.PORT || 3000
+const bodyParser = require('body-parser');
+const api_token = process.env.api_token
+const integrator_id = process.env.integrator_id
 
+mercadopago.configure({
+    access_token: api_token,
+    integrator_id: integrator_id,
+});
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
 
-app.get('/', function (req, res) {
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/detail', function (req, res) {
+app.get('/detail', function(req, res) {
     res.render('detail', req.query);
 });
+
+app.get('/pending', function(req, res) {
+    res.render('pending', req.query);
+});
+app.get('/success', function(req, res) {
+    res.render('success', req.query);
+});
+app.get('/fail', function(req, res) {
+    res.render('fail', req.query);
+});
+
+app.post('/paid', function(req, res) {
+    let preference = req.body;
+    preference.items.forEach(element => {
+        element.unit_price = parseFloat(element.unit_price)
+        element.quantity = parseInt(element.quantity)
+    });
+    preference.payer.phone.number = parseInt(preference.payer.phone.number)
+    preference.payer.address.street_number = parseInt(preference.payer.address.street_number)
+    preference.payment_methods.installments = parseInt(preference.payment_methods.installments)
+    mercadopago.preferences.create(req.body)
+        .then(function(response) {
+            // Este valor reemplazará el string "<%= global.id %>"
+            // en tu HTML
+            global.id = response.body.id;
+            res.json(response.body)
+        }).catch(function(error) {
+            console.log(error);
+            res.json(error)
+        });
+});
+
+app.post('/notification', async function(req, res) {
+    const webhook = req.body;
+    switch (webhook.type) {
+        case "payment":
+            mercadopago.payment.findById(webhook.id)
+            res.json({ hola: "mundo" })
+            break;
+        case "plan":
+            mercadopago.plan.findById(webhook.id)
+            break;
+        case "subscription":
+            mercadopago.subscription.findById(webhook.id)
+            break;
+        case "invoice":
+            mercadopago.invoice.findById(webhook.id)
+            break;
+
+        default:
+            break;
+    }
+});
+
 
 app.listen(port);
---
+++ assets/mercadopago.js
@@ -0,0 +1,73 @@
+window.onload = function() {
+    const mp = new MercadoPago('APP_USR-a98b17ae-47a6-4a35-b92d-01919002b97e', {
+        locale: 'es-CO'
+    });
+    pagar(mp)
+};
+var payer = {
+    name: "Lalo",
+    surname: "Landa",
+    email: "test_user_83958037@testuser.com",
+    phone: {
+        area_code: "52",
+        number: "5549737300"
+    },
+    address: {
+        street_name: "Insurgentes Sur",
+        street_number: "4602",
+        zip_code: "03940"
+    }
+}
+
+
+function pagar(mp) {
+    let producto = {
+        items: [{
+            id: $("#id").val(),
+            picture_url: window.location.hostname + ":3000" + $("#img").val(),
+            description: "Dispositivo móvil de Tienda e-commerce",
+            title: $("#title").val(),
+            unit_price: $("#price").val(),
+            quantity: $("#unit").val(),
+        }],
+        payer: payer,
+        back_urls: {
+            success: window.location.hostname + ":3000" + "/success",
+            failure: window.location.hostname + ":3000" + "/fail",
+            pending: window.location.hostname + ":3000" + "/pending"
+        },
+        auto_return: "approved",
+        payment_methods: {
+            excluded_payment_methods: [{
+                id: "amex"
+            }],
+            excluded_payment_types: [{
+                id: "atm"
+            }],
+            installments: 6,
+        },
+        external_reference: "burbinagomez@gmail.com",
+        notification_url: window.location.hostname + ":3000" + "/notification",
+    };
+    $.ajax({
+        url: "/paid",
+        type: "POST",
+        contentType: 'application/json',
+        data: JSON.stringify(producto),
+        success: function(respuesta) {
+            mp.checkout({
+                preference: {
+                    id: respuesta.id
+                },
+                render: {
+                    container: '#pagar', // Indica dónde se mostrará el botón de pago
+                    label: 'Pagar', // Cambia el texto del botón de pago (opcional)
+                }
+            });
+        },
+        error: function() {
+            console.log("No se ha podido obtener la información");
+        }
+    });
+
+}
